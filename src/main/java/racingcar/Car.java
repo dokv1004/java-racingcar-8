edd7c0b322e94be4920eb301d48896d33529e56f
devJSH1004 <devJSH1004@gmail.com>
2025-10-27 18:09:45 +0900

    [FEAT] Car 클래스 이름 입력, 위치 계산, 위치 출력 기능 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,35 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void nowPosition() {
+        int num = Randoms.pickNumberInRange(0,9);
+        if (num >= 4) {
+            position++;
+        }
+    }
+
+    public void printPosition() {
+        System.out.print(this.name + " : ");
+        for (int i=0; i<this.position; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
 }
