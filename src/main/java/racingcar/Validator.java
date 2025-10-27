954a92233f2c81cd34a06e9757c693beced8184e
devJSH1004 <devJSH1004@gmail.com>
2025-10-27 21:18:49 +0900

    [FEAT] Validator 클래스 게임 횟수 관련 유효성 검증 기능 추가

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -29,13 +29,24 @@ public class Validator {
 
     public void isNameValidated(List<String> names) {
         if (!isValidatedPlayerNum(names)) {
-            throw new IllegalArgumentException("두 명이상의 플레이어가 필요합니다.");
+            throw new IllegalArgumentException("두 명 이상의 플레이어가 필요합니다.");
         }
         if (!isValidatedNameLength(names)) {
             throw new IllegalArgumentException("플레이어 이름은 5자 이하여야 합니다.");
         }
         if (!isValidatedDuplication(names)) {
-            throw new IllegalArgumentException("중복된 이름 존재");
+            throw new IllegalArgumentException("중복된 플레이어의 이름이 존재헙니다.");
+        }
+    }
+
+    public void isTimeValidated(String playTimes) {
+        try {
+            int times = Integer.parseInt(playTimes);
+            if (times <= 0) {
+                throw new IllegalArgumentException("1 이상의 숫자를 입력해주세요.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("1 이상의 숫자를 입력해주세요.");
         }
     }
 }
